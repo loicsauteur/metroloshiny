c06ec9b6c517ae7e9fee9d0ebacb438c74dcfe22
Loïc Sauteur <loic.sauteur@unibas.ch>
2026-07-13 09:38:04 +0200

    Add Field distortion/uniformity place-holder

---
+++ src/metroloshiny/metroloshiny_apps/distortion_uniformity/app.py
@@ -0,0 +1,9 @@
+# for upload: table with omero IDs per date
+#     (ie. upload check if the rois are available/ 'Field_Uniformity_avg', 'Field_Distortion_avg__um' != 1)
+# for app: load ROIS and visualise them?
+# Distortion based on point coordinates vs ideal position
+# Uniformity based on point mean intensity
+# analysis based on: https://github.com/BIOP/ArgoLight_analysis_tool/blob/988c4147822562c25c53a04ad20fad577798aeed/scripts/ARGO-SIM_analysis_code.groovy
+# check also this for adding ROI uploads: https://github.com/BIOP/ArgoLight_analysis_tool/blob/988c4147822562c25c53a04ad20fad577798aeed/src/main/java/ch/epfl/biop/processing/ArgoSlideProcessing.java
+# ---------
+# user Image ID = 3021627
